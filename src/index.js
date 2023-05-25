a5443996a91d7b735ac0c49e603cef1e7bf9f4ea
YuraBuchak <115108617+YuraBuchak@users.noreply.github.com>
2023-05-25 15:19:06 +0300

    useState, useEffect

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+import { AppHooks } from 'components/AppHooks';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppHooks />
   </React.StrictMode>
 );
